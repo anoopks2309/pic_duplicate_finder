1aa7faa5f2ae9bc36909b12dc574827bb710369f
anoopks2309 <anoop0923@gmail.com>
2017-02-11 12:22:39 +0530

    java code to find the uniques images with the help of sequential file

---
+++ Unique_images/src/packegee/Mydriver.java
@@ -0,0 +1,38 @@
+package packegee;
+
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class Mydriver {
+
+	public static void main(String[] args) throws Exception {
+
+		Configuration conf = new Configuration();	
+
+		Job job = new Job(conf, "map reduce to search duplicate images from sequence files");
+		job.setJarByClass(Mydriver.class);
+		job.setNumReduceTasks(1);
+		job.setMapperClass(Mymapper.class);
+		job.setReducerClass(Myreducer.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setInputFormatClass(SequenceFileInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+}
+
+
